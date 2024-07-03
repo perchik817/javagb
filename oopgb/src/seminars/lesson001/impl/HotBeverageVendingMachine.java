57c233ad497d4db102d08c9f481c35d91f17bedd
Perchik <saikalrustamovna@gmail.com>
2024-07-03 18:23:52 +0600

    oop seminar001 hw v2

--- oopgb/src/seminars/lesson001/impl/HotBeverageVendingMachine.java
+++ oopgb/src/seminars/lesson001/impl/HotBeverageVendingMachine.java
@@ -3,18 +3,35 @@ package seminars.lesson001.impl;
 import seminars.lesson001.Product;
 import seminars.lesson001.VendingMachine;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HotBeverageVendingMachine extends VendingMachine {
     public HotBeverageVendingMachine() {}
 
-    //@Override
     public Product getProduct(String name, double volume, int temperature){
-        return getProducts().stream()
-                .filter(product -> product instanceof HotBeverage)
-                .map(product -> (HotBeverage) product)
-                .filter(hotBeverage -> hotBeverage.getName().equals(name) &&
+        //simple without using threads
+        for (Product product : getProducts()) {
+            if (product instanceof HotBeverage) {
+                HotBeverage hotBeverage = (HotBeverage) product;
+                if (hotBeverage.getName().equals(name) &&
                         hotBeverage.getVolume() == volume &&
-                        hotBeverage.getTemperature() == temperature)
-                .findFirst()
-                .orElse(null);
+                        hotBeverage.getTemperature() == temperature) {
+                    return hotBeverage;
+                }
+            }
+        }
+        return null;
+
+//        return getProducts().stream()
+//                .filter(product -> product instanceof HotBeverage)
+//                .map(product -> (HotBeverage) product)
+//                .filter(hotBeverage -> hotBeverage.getName().equals(name) &&
+//                        hotBeverage.getVolume() == volume &&
+//                        hotBeverage.getTemperature() == temperature)
+//                .findFirst()
+//                .orElse(null);
+
+
     }
 }
