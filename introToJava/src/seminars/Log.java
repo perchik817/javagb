b29e9ff98f7cdb7eb9901192af69f5ce154c5159
Perchik <saikalrustamovna@gmail.com>
2024-09-19 13:34:28 +0600

    intro to java:seminars:l2 - work with logs

---
+++ introToJava/src/seminars/Log.java
@@ -0,0 +1,19 @@
+package seminars;
+
+import java.io.FileInputStream;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+public class Log {
+    static {
+        try(FileInputStream in = new FileInputStream("log.config")){ //относительный путь до файла с конфигами
+            LogManager.getLogManager().readConfiguration(in);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Logger log(String className) {
+        return Logger.getLogger(className);
+    }
+}
