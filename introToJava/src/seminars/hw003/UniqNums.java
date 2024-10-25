0b9d5b4ad7974f631657452e30ab7e9784880d44
Perchik <saikalrustamovna@gmail.com>
2024-10-25 11:51:04 +0600

    intro to java:seminars:hw003:task2

--- introToJava/src/seminars/hw003/UniqNums.java
+++ introToJava/src/seminars/hw003/UniqNums.java
@@ -1,5 +1,6 @@
 package seminars.hw003;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -9,5 +10,23 @@ public class UniqNums {
         int[] a = new int[]{1, 2, 2, 3, 4, 4, 5};
         Set<Integer> uniqueNums = Arrays.stream(a).boxed().collect(Collectors.toSet());
         System.out.println(uniqueNums);
+        //second variant
+        int[] result = getUniqueElements(a);
+        System.out.println(Arrays.toString(result));
     }
+
+    public static int[] getUniqueElements(int[] a) {
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int num : a) {
+            if (!result.contains(num)) {
+                result.add(num);
+            }
+        }
+        int[] resultArray = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            resultArray[i] = result.get(i);
+        }
+        return resultArray;
+    }
+
 }
