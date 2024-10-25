bcf59316cb42d0776695a1a4bee55b7aa49b910d
Perchik <saikalrustamovna@gmail.com>
2024-10-25 11:47:42 +0600

    introtojava:seminars:hw003:task2

--- introToJava/src/seminars/hw003/UniqNums.java
+++ introToJava/src/seminars/hw003/UniqNums.java
@@ -1,2 +1,13 @@
-package seminars.hw003;public class UniqNums {
+package seminars.hw003;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class UniqNums {
+    public static void main(String[] args) {
+        int[] a = new int[]{1, 2, 2, 3, 4, 4, 5};
+        Set<Integer> uniqueNums = Arrays.stream(a).boxed().collect(Collectors.toSet());
+        System.out.println(uniqueNums);
+    }
 }
